da350d2faf8707f8fcba3e9a90c0fe72712c5557
Simonas Mulevicius <mulevicius.simonas@gmail.com>
2019-10-22 20:37:45 +0100

    Task 6

--- resources/fragment_shader.glsl
+++ resources/fragment_shader.glsl
@@ -46,21 +46,24 @@ vec3 getRayDir() {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-float sphere(vec3 pt) {
-  return length(pt) - 1;
+float sphere(vec3 pt, float radius) {
+    return length(pt) - radius;
 }
 
-//Non-linear cube
 float cubeNonLinear(vec3 pt) {
   return max(max(abs(pt.x), abs(pt.y)), abs(pt.z)) - 1;
 }
 
-//Linear cube
 float cubeLinear(vec3 pt) {
   vec3 q = abs(pt) - vec3(1);
 
   //Why sould we write length(max()) instead of max(length(), 0)
   return length(max(q, 0.0)) + min(max(q.x, max(q.y, q.z)), 0.0);
+}
+
+float roundBox( vec3 p, vec3 b, float r ) {
+    vec3 q = abs(p) - b;
+    return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0) - r;
 }
 
 float plane(vec3 pt) {
@@ -69,6 +72,48 @@ float plane(vec3 pt) {
 
 float torus(vec3 pt, float majorRadius, float minorRadius){
     return length(vec2(length(pt.xz) - majorRadius , pt.y)) - minorRadius;
+}
+
+float capsule( vec3 pt, vec3 a, vec3 b, float r )
+{
+    vec3 pa = pt - a, ba = b - a;
+    float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
+    return length( pa - ba*h ) - r;
+}
+
+float hexPrism( vec3 p, vec2 h )
+{
+    const vec3 k = vec3(-0.8660254, 0.5, 0.57735);
+    p = abs(p);
+    p.xy -= 2.0*min(dot(k.xy, p.xy), 0.0)*k.xy;
+    vec2 d = vec2(
+    length(p.xy-vec2(clamp(p.x,-k.z*h.x,k.z*h.x), h.x))*sign(p.y-h.x),
+    p.z-h.y );
+    return min(max(d.x,d.y),0.0) + length(max(d,0.0));
+}
+
+float dot2( in vec3 v ) { return dot(v,v); }
+float udQuad( vec3 p, vec3 a, vec3 b, vec3 c, vec3 d )
+{
+    vec3 ba = b - a; vec3 pa = p - a;
+    vec3 cb = c - b; vec3 pb = p - b;
+    vec3 dc = d - c; vec3 pc = p - c;
+    vec3 ad = a - d; vec3 pd = p - d;
+    vec3 nor = cross( ba, ad );
+
+    return sqrt(
+    (sign(dot(cross(ba,nor),pa)) +
+    sign(dot(cross(cb,nor),pb)) +
+    sign(dot(cross(dc,nor),pc)) +
+    sign(dot(cross(ad,nor),pd))<3.0)
+    ?
+    min( min( min(
+    dot2(ba*clamp(dot(ba,pa)/dot2(ba),0.0,1.0)-pa),
+    dot2(cb*clamp(dot(cb,pb)/dot2(cb),0.0,1.0)-pb) ),
+    dot2(dc*clamp(dot(dc,pc)/dot2(dc),0.0,1.0)-pc) ),
+    dot2(ad*clamp(dot(ad,pd)/dot2(ad),0.0,1.0)-pd) )
+    :
+    dot(nor,pa)*dot(nor,pa)/dot2(nor) );
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -108,7 +153,55 @@ float intersection (float sdf1, float sdf2){
 }
 
 float sdfWithoutPlane(vec3 pt) {
-    return torus(rotationAboutXAxis( translation(pt, vec3(0, 3, 0)) , PI/2) , 3, 1);
+    //bool isWithinBorders = ((pt.x >= -10 && pt.x < 9) && (pt.y >= -10 && pt.y < 9));
+    float fence =
+            difference(
+                intersection(
+                roundBox(pt, vec3(20, 2, 30), 0),
+                roundBox(vec3(mod(pt.x + 2, 4) - 2, pt.y, mod(pt.z + 2, 4) - 2), vec3(1, 2, 1), 0)),
+                roundBox(pt, vec3(18, 3, 28), 0)
+            );
+
+    float hill = blend(
+                    blend(
+                        sphere(pt, 5),
+                        sphere(translation(pt, vec3(0, 0, 4)), 4)
+                    ),
+                    blend(
+                        blend(
+                            sphere(translation(pt, vec3(-3, 0, -3)), 3.5),
+                            sphere(translation(pt, vec3(5, 0, 2)), 4)
+                            ),
+                        blend(
+                            sphere(translation(pt, vec3(2, 0, -6)), 4),
+                            sphere(translation(pt, vec3(-4, 0, 5)), 3.5)
+                        )
+                    )
+            );
+
+    float castleLayer1 = hexPrism(rotationAboutXAxis(translation(pt, vec3(0, 0, 10)), PI/2), vec2(5,2));
+    float castleLayer2 = hexPrism(rotationAboutXAxis(translation(pt, vec3(0, 2, 10)), PI/2), vec2(4,2));
+    float castleLayer3 = hexPrism(rotationAboutXAxis(translation(pt, vec3(0, 4, 10)), PI/2), vec2(3,2));
+    float castleLayer4 = hexPrism(rotationAboutXAxis(translation(pt, vec3(0, 4, 10)), PI/2), vec2(2.5,3));
+
+    float castle = unification(
+            unification(castleLayer1, castleLayer2),
+            difference(castleLayer3, castleLayer4)
+    );
+
+    float flag = unification(
+        capsule(pt, vec3(0,0,10), vec3(0,10,10), 0.25),
+        udQuad(pt, vec3(0,10,10), vec3(0,12,10), vec3(0,12,14), vec3(0,10,14)));
+    return unification(
+            fence,
+            blend(
+                unification(
+                    castle,
+                    flag ),
+                hill
+            )
+    );
+    //return torus(rotationAboutXAxis( translation(pt, vec3(0, 3, 0)) , PI/2) , 3, 1);
 }
 
 float sdfWithPlane(vec3 pt) {
